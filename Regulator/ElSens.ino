06e45f01ff9ed7f7a675b9b6ff461b85d71f24a2
Juraj Andrassy <juraj.andrassy@gmail.com>
2018-05-13 21:07:41 +0200

    ElSens tuning

--- Regulator/ElSens.ino
+++ Regulator/ElSens.ino
@@ -1,6 +1,6 @@
 
-const int ELSENS_MIN_ON_VALUE = 580;
-const int ELSENS_MIN_HEATING_VALUE = ELSENS_MIN_ON_VALUE + 200;
+const int ELSENS_MIN_ON_VALUE = 110;
+const int ELSENS_MIN_HEATING_VALUE = 800;
 const unsigned long OVERHEATED_COOLDOWN_TIME = PUMP_STOP_MILLIS - 30000; // resume 30 sec before pump stops
 
 unsigned long overheatedStart = 0;
@@ -14,7 +14,7 @@ void elsensLoop() {
     if ((loopStartMillis - overheatedStart) < OVERHEATED_COOLDOWN_TIME && !buttonPressed)
       return;
     overheatedStart = 0;
-    state = RegulatorState::REGULATING;
+    state = RegulatorState::MONITORING;
   }
 
   if (heatingPower > 0 && elsens < ELSENS_MIN_HEATING_VALUE) {
@@ -25,8 +25,12 @@ void elsensLoop() {
     alarmSound();
     return;
   }
-  float ratio = (float) elsens / 10600;
-  elsensPower = (int) ((float) elsens * 0.2 * sin(ratio * PI/2));
+  if (heatingPower == 0 || bypassRelayOn) {
+    elsensPower = 0;
+  } else {
+    float ratio = (float) elsens / 2400;
+    elsensPower = (int) ((float) elsens * sin(ratio * PI/2));
+  }
 }
 
 boolean elsensCheckPump() {
@@ -46,13 +50,33 @@ byte overheatedSecondsLeft() {
   return (OVERHEATED_COOLDOWN_TIME - (loopStartMillis - overheatedStart)) / 1000;
 }
 
+/**
+ * Grove Electricity Sensor module removes the negative part of the AC oscillation
+ * zero crossing is where the removed part ends (sequence of zero readings)
+ */
 int readElSens() {
+
+  // wait for zero crossing
+  byte countOf0 = 0;
+  long start_time = millis();
+  while (millis() - start_time < 40) {
+    int v = analogRead(ELSENS_PIN);
+    if (v > 0 && countOf0 > 10)
+      break;
+    if (v == 0) {
+      countOf0++;
+    }
+  }
+  if (countOf0 < 10) // sensor is not connected, pin is floating
+    return ELSENS_MIN_ON_VALUE;
+
+  // sample AC
   long sum = 0;
   int n = 0;
-  long start_time = millis();
+  start_time = millis();
   while(millis() - start_time < 400) { // in 400 ms measures 20 50Hz AC oscillations
     sum += analogRead(ELSENS_PIN);
     n++;
   }
-  return sum * 100 / n;
+  return sum * 10 / (n / 2); // half of the values are zeros for removed negative voltages
 }
