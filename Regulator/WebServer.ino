eb68512d9db766928e0967306631882e172ccc7f
Juraj Andrassy <juraj.andrassy@gmail.com>
2018-05-15 19:32:06 +0200

    WebServer - extracted rest and serve functions from loop

--- Regulator/WebServer.ino
+++ Regulator/WebServer.ino
@@ -12,11 +12,11 @@ enum struct RestRequest {
 
 NetServer webServer(81);
 
-void restServerSetup() {
+void webServerSetup() {
   webServer.begin();
 }
 
-void restServerLoop() {
+void webServerLoop() {
 
   static NetClient client = webServer.available();
 
@@ -36,98 +36,109 @@ void restServerLoop() {
       }
       msg.print(fn);
 #ifdef ethernet_h
-      char buff[150];
+      char buff[256];
 #else
       char buff[64];
 #endif
+      ChunkedPrint chunked(client, buff, sizeof(buff));
       if (l == 1 && strchr_P((const char*) F("IELAPHVS"), fn[0])) {
-        RestRequest request = (RestRequest) fn[0];
-        ChunkedPrint chunked(client, buff, sizeof(buff));
-        chunked.println(F("HTTP/1.1 200 OK"));
-        chunked.println(F("Content-Type: application/json"));
-        chunked.println(F("Transfer-Encoding: chunked"));
-        chunked.println(F("Cache-Control: no-store"));
-        chunked.println(F("Access-Control-Allow-Origin: *"));
-        chunked.println();
-        chunked.begin();
-        switch (request) {
-          default:
-            printValuesJson(chunked);
-            break;
-          case RestRequest::EVENTS:
-            eventsPrintJson(chunked);
-            break;
-          case RestRequest::CSV_LIST:
-            csvLogPrintJson(chunked);
-            break;
-          case RestRequest::ALARM:
-            printAlarmJson(chunked);
-            break;
-          case RestRequest::PUMP_ALARM_RESET:
-            buttonPressed = true;
-            break;
-          case RestRequest::MANUAL_RUN:
-            manualRunRequest = true;
-            break;
-          case RestRequest::VALVES_BACK:
-            valvesBackStart(0);
-            break;
-          case RestRequest::SAVE_EVENTS:
-            eventsSave();
-            break;
-        }
-        chunked.end();
+        webServerRestRequest(fn, chunked);
       } else {
-        BufferedPrint bp(client, buff, sizeof(buff));
-        boolean notFound = true;
-#ifdef __SD_H__
-        if (sdCardAvailable) {
-          char* ext = strchr(fn, '.');
-          if (strlen(ext) > 4) {
-            ext[4] = 0;
-            memmove(ext + 2, ext, 5);
-            ext[0] = '~';
-            ext[1] = '1';
-            ext += 2;
-          }
-          File dataFile = SD.open(fn);
-          if (dataFile) {
-            notFound = false;
-            bp.println(F("HTTP/1.1 200 OK"));
-            bp.print(F("Content-Type: "));
-            bp.println(getContentType(ext));
-            if (strcmp(ext, ".csv") == 0) {
-              bp.println(F("Content-Disposition: attachment"));
-            }
-            bp.print(F("Content-Length: "));
-            bp.println(dataFile.size());
-            unsigned long expires = now() + SECS_PER_YEAR;
-            bp.printf(F("Expires: %s, "), dayShortStr(weekday(expires))); // two printfs because ShortStr functions share the buffer
-            bp.printf(F("%d %s %d 00:00:00 GMT"), day(expires), monthShortStr(month(expires)), year(expires));
-            bp.println();
-            bp.println();
-            while (dataFile.available()) {
-              bp.write(dataFile.read());
-            }
-            dataFile.close();
-            bp.flush();
-          }
-        }
-#endif
-        if (notFound) {
-          bp.println(F("HTTP/1.1 404 Not Found"));
-          bp.printf(F("Content-Length: "));
-          bp.println(12 + strlen(fn));
-          bp.println();
-          bp.printf(F("\"%s\" not found"), fn);
-          bp.flush();
-        }
+        webServerServeFile(fn, chunked);
       }
     }
   } else {
     client.stop();
   }
 }
+
+void webServerRestRequest(const char *fn, ChunkedPrint& chunked) {
+  RestRequest request = (RestRequest) fn[0];
+  chunked.println(F("HTTP/1.1 200 OK"));
+  chunked.println(F("Connection: close"));
+  chunked.println(F("Content-Type: application/json"));
+  chunked.println(F("Transfer-Encoding: chunked"));
+  chunked.println(F("Cache-Control: no-store"));
+  chunked.println(F("Access-Control-Allow-Origin: *"));
+  chunked.println();
+  chunked.begin();
+  switch (request) {
+    default:
+      printValuesJson(chunked);
+      break;
+    case RestRequest::EVENTS:
+      eventsPrintJson(chunked);
+      break;
+    case RestRequest::CSV_LIST:
+      csvLogPrintJson(chunked);
+      break;
+    case RestRequest::ALARM:
+      printAlarmJson(chunked);
+      break;
+    case RestRequest::PUMP_ALARM_RESET:
+      buttonPressed = true;
+      break;
+    case RestRequest::MANUAL_RUN:
+      manualRunRequest = true;
+      break;
+    case RestRequest::VALVES_BACK:
+      valvesBackStart(0);
+      break;
+    case RestRequest::SAVE_EVENTS:
+      eventsSave();
+      break;
+  }
+  chunked.end();
+}
+
+void webServerServeFile(const char *fn, BufferedPrint& bp) {
+  boolean notFound = true;
+#ifdef __SD_H__
+  if (sdCardAvailable) {
+    char* ext = strchr(fn, '.');
+    if (strlen(ext) > 4) {
+      ext[4] = 0;
+      memmove(ext + 2, ext, 5);
+      ext[0] = '~';
+      ext[1] = '1';
+      ext += 2;
+    }
+    File dataFile = SD.open(fn);
+    if (dataFile) {
+      notFound = false;
+      bp.println(F("HTTP/1.1 200 OK"));
+      bp.println(F("Connection: close"));
+      bp.print(F("Content-Type: "));
+      bp.println(getContentType(ext));
+      if (strcmp(ext, ".csv") == 0) {
+        bp.println(F("Content-Disposition: attachment"));
+      }
+      bp.print(F("Content-Length: "));
+      bp.println(dataFile.size());
+      unsigned long expires = now() + SECS_PER_YEAR;
+      bp.printf(F("Expires: %s, "), dayShortStr(weekday(expires))); // two printfs because ShortStr functions share the buffer
+      bp.printf(F("%d %s %d 00:00:00 GMT"), day(expires), monthShortStr(month(expires)), year(expires));
+      bp.println();
+      bp.println();
+      while (dataFile.available()) {
+        bp.write(dataFile.read());
+      }
+      dataFile.close();
+      bp.flush();
+    }
+  }
+#endif
+  if (notFound) {
+    bp.println(F("HTTP/1.1 404 Not Found"));
+    bp.printf(F("Content-Length: "));
+    bp.println(12 + strlen(fn));
+    bp.println();
+    bp.printf(F("\"%s\" not found"), fn);
+    bp.flush();
+  }
+}
+
+
 
 void printValuesJson(FormattedPrint& client) {
   client.printf(F("{\"st\":\"%c\",\"v\":\"%s\",\"r\":\"%d %d %d %d\",\"ec\":%d,\"ts\":%d"),
