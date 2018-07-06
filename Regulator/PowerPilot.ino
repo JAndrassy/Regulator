499fbaf3c4a8e4dc9cca9f0edafb258f49bdf3d7
Juraj Andrassy <juraj.andrassy@gmail.com>
2018-07-06 19:38:51 +0200

    PowerPilot - pwm value calculated with function

--- Regulator/PowerPilot.ino
+++ Regulator/PowerPilot.ino
@@ -1,20 +1,20 @@
-
-const byte MAP_POINTS_COUNT = 7;
-unsigned short power2pwmPoints[MAP_POINTS_COUNT][2] = {
-  { 500, 223},
-  { 630, 243},
-  {1140, 363},
-  {1300, 443},
-  {1740, 683},
-  {1900, 863},
-  {MAX_POWER, 1023}
-};
-const int MIN_POWER = power2pwmPoints[0][0];
+//const byte MAP_POINTS_COUNT = 7;
+//unsigned short power2pwmPoints[MAP_POINTS_COUNT][2] = {
+//  { 500, 223},
+//  { 630, 243},
+//  {1140, 363},
+//  {1300, 443},
+//  {1740, 683},
+//  {1900, 863},
+//  {MAX_POWER, 1023}
+//};
+//const int MIN_POWER = power2pwmPoints[0][0];
+const int MIN_POWER = 300;
 
 void pilotLoop() {
 
   const byte MONITORING_UNTIL_SOC = 85; // %
-  const int MIN_START_POWER = MIN_POWER + 200;
+  const int MIN_START_POWER = 600;
   const int BYPASS_MIN_START_POWER = BYPASS_POWER + 100;
   const byte WAIT_FOR_IT_COUNT = 2;
 
@@ -37,7 +37,7 @@ void pilotLoop() {
     return;
 
   // sum available power
-  availablePower = heatingPower + m + ((b > 0 || soc > 97) ? 0 : b) - (mainRelayOn ? 0 : PUMP_POWER);
+  availablePower = heatingPower + m + (b > 0 ? 0 : b) - (mainRelayOn ? 0 : PUMP_POWER);
   if (heatingPower == 0 && availablePower < MIN_START_POWER) {
     waitForItCounter = 0;
     return;
@@ -75,19 +75,40 @@ void pilotLoop() {
 }
 
 unsigned short power2pwm(int power) {
+
+  const float POWER2PWM_KOEF = 0.16;
+  const float PF_ANGLE_SHIFT = 0.05 * PI;
+  const float PF_ANGLE_INTERVAL = 0.33 * PI;
+  const unsigned short MIN_PWM = 110;
+
   if (power < MIN_POWER)
     return 0;
-  if (power >= MAX_POWER)
-    return power2pwmPoints[MAP_POINTS_COUNT - 1][1];
-  int i = 0;
-  for (; i < MAP_POINTS_COUNT - 1; i++) {
-    if (power >= power2pwmPoints[i][0] && power < power2pwmPoints[i + 1][0])
-      break;
+  unsigned short res;
+  if (power >= MAX_POWER) {
+    res = 1023;
+  } else {
+    float ratio = (float) power / MAX_POWER;
+    res = MIN_PWM + POWER2PWM_KOEF * power / cos(PF_ANGLE_SHIFT + (ratio * PF_ANGLE_INTERVAL));
   }
-  unsigned short res = map(power, power2pwmPoints[i][0], power2pwmPoints[i + 1][0], power2pwmPoints[i][1], power2pwmPoints[i+1][1]);
 #ifndef ESP8266
   res /= 4;
 #endif
   return res;
 }
 
+//unsigned short power2pwm(int power) {
+//  if (power < MIN_POWER)
+//    return 0;
+//  if (power >= MAX_POWER)
+//    return power2pwmPoints[MAP_POINTS_COUNT - 1][1];
+//  int i = 0;
+//  for (; i < MAP_POINTS_COUNT - 1; i++) {
+//    if (power >= power2pwmPoints[i][0] && power < power2pwmPoints[i + 1][0])
+//      break;
+//  }
+//  unsigned short res = map(power, power2pwmPoints[i][0], power2pwmPoints[i + 1][0], power2pwmPoints[i][1], power2pwmPoints[i+1][1]);
+//#ifndef ESP8266
+//  res /= 4;
+//#endif
+//  return res;
+//}
