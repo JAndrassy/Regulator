5be474c3cfc2279b59a478f93f26e217331ca70e
Juraj Andrassy <juraj.andrassy@gmail.com>
2019-08-19 18:49:20 +0200

    SusCalib - Digest auth MD5 response calculation

--- Regulator/SusCalib.ino
+++ Regulator/SusCalib.ino
@@ -1,16 +1,23 @@
+#include <MD5.h> // https://github.com/tzikis/ArduinoMD5
+
+#define FSH_P const __FlashStringHelper*
 
 const char uri[] PROGMEM = "/servicecgi-bin/suspend_battery_calibration/?method=save"; // is used twice
 const char data[] PROGMEM = "{\"suspension_time_seconds\":10800}"; // 3 hours
+const char USERNAME_P[] PROGMEM = "service";
+const char REALM_P[] PROGMEM = "Webinterface area";
 
 void susCalibLoop() {
 
   const byte SUSCALIB_HOUR = 9;
   const byte SUSCALIB_SOC_LIMIT = 90;
   static boolean done = false;
+  static char nonce[64] = "";
 
   if (hourNow != SUSCALIB_HOUR) {
     if (done) {
       done = false;
+      nonce[0] = 0;
     }
     return;
   }
@@ -19,28 +26,57 @@ void susCalibLoop() {
   if (pvSOC > SUSCALIB_SOC_LIMIT) // over night calibration in progress
     return;
 
+  char buff[128];
+  char authResponse[33] = "";
+  if (strlen(nonce)) {
+    char ha2[33];
+    strcpy_P(buff, PSTR("POST:")); // build HA2
+    strcat_P(buff, uri);
+    md5HashHex(ha2, buff);  // HA2 is HEX md5 of method:URI
+    strcpy_P(buff, USERNAME_P); // build HA1
+    strcat(buff, ":");
+    strcat_P(buff, REALM_P);
+    strcat(buff, ":");
+    strcat(buff, SYMO_SERVICE_PASS);
+    md5HashHex(buff, buff); // HA1 starts the response string
+    strcat(buff, ":");
+    strcat(buff, nonce);
+    strcat(buff, ":");
+    strcat(buff, ha2);
+    md5HashHex(authResponse, buff);
+  }
+
   int st = -1;
   NetClient client;
 
   if (client.connect(symoAddress, 80)) {
     st = 0;
 
-    char buff[64];
     BufferedPrint bp(client, buff, sizeof(buff));
 
     bp.print(F("POST "));
-    bp.print((__FlashStringHelper*) uri); // FlashStringHelper cast to call the PROGMEM version of print
+    bp.print((FSH_P) uri);
     bp.println(F(" HTTP/1.1"));
     bp.print(F("Host: "));
     bp.println(symoAddress);
     bp.println(F("Connection: close"));
     bp.print(F("Content-Length: "));
     bp.println(strlen_P(data));
-    bp.print(F("Authorization: Digest username=\"service\",realm=\"W\",nonce=\"ffcb243c6f951a5bab771e9d3b8f81d6\",uri=\""));
-    bp.print((__FlashStringHelper*) uri);
-    bp.println(F("\",response=\"" SUSCALIB_DIGEST_RESPONSE "\"")); // SUSCALIB_DIGEST_RESPONSE is in secrets.h
+    if (strlen(authResponse)) {
+      bp.print(F("Authorization: Digest username=\""));
+      bp.print((FSH_P) USERNAME_P);
+      bp.print(F("\",realm=\""));
+      bp.print((FSH_P) REALM_P);
+      bp.print(F("\",nonce=\""));
+      bp.print(nonce);
+      bp.print(F("\",uri=\""));
+      bp.print((FSH_P) uri);
+      bp.print(F("\",response=\""));
+      bp.print(authResponse);
+      bp.println('"');
+    }
     bp.println();
-    bp.print((__FlashStringHelper*) data);
+    bp.print((FSH_P) data);
     bp.flush();
 
     client.setTimeout(8000);
@@ -59,10 +95,44 @@ void susCalibLoop() {
     } else {
       st = -2;
     }
+    if (st == 401) {
+      nonce[0] = 0;
+      if (client.find((char*) "nonce=\"")) {
+        int l = client.readBytesUntil('"', nonce, sizeof(nonce));
+        if (l) {
+          nonce[l] = 0;
+        } else {
+          st = -6;
+        }
+      } else {
+        st = -5;
+      }
+    }
     client.setTimeout(10);
     while (client.readBytes(buff, sizeof(buff)) != 0); // read the rest
     client.stop();
   }
+ if (st != 401) {
   eventsWrite(SUSPEND_CALIBRATION_EVENT, st, 0);
   done = true;
+ }
+}
+
+void md5HashHex(char* dest, const char* str) {
+
+  static const char hexDigits[] = "0123456789abcdef";
+
+  int len = 16;
+  byte hash[len];
+
+  MD5_CTX context;
+  MD5::MD5Init(&context);
+  MD5::MD5Update(&context, str, strlen(str));
+  MD5::MD5Final(hash, &context);
+
+  for (int i = 0; i < len; i++) {
+    dest[i * 2] = hexDigits[hash[i] >> 4];
+    dest[(i * 2) + 1] = hexDigits[hash[i] & 0x0F];
+  }
+  dest[len * 2] = '\0';
 }
--- Regulator/secrets.h
+++ Regulator/secrets.h
@@ -4,9 +4,5 @@ byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 #endif
 
 #define MY_BLYNK_TOKEN "YourBlynkToken"
-#define SUSCALIB_DIGEST_RESPONSE "DigestAuthResponse"
-
-//ha1 = md5("service:W:PASSWORD") // replace PASSWORD
-//ha2 = md5("POST:/servicecgi-bin/suspend_battery_calibration/?method=save")
-//response = md5(ha1 + ":ffcb243c6f951a5bab771e9d3b8f81d6:" + ha2)
+#define SYMO_SERVICE_PASS "yourPassword"
 
